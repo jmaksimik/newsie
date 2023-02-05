0304f87115132e23e1c266f48c8b396f562dc021
jmaksimik <jmaks@me.com>
2023-02-05 18:13:01 -0500

    Basic landing page layout

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,18 +1,50 @@
 import React from 'react';
+import { Grid, Segment, List, Button } from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+
 
 import PageHeader from '../../components/PageHeader/PageHeader';
+import ImageCarousel from '../../components/ImageCarousel/ImageCarousel';
 
 export default function LandingPage({ handleUserState, loggedUser }) {
     return (
         <>
             <PageHeader loggedUser={loggedUser} />
-            <div class='ui inverted vertical masthead center aligned segment'>
-                <div class='ui text container'>
-                    <h1>Welcome to Newsie!</h1>
-                    <h2>Follow What Matters to You</h2>
+            <Grid 
+                textAlign='center'
+                style={{height: '100vh', width: '100vw'}}
+                verticalAlign='top'
+                >
+                <Grid.Column>
+                    <div className='landing-title'>
+                        <h1>Welcome to Newsie!</h1>
+                        <h2>Follow What Matters to You</h2>
+                    </div>
+                <div className='landing-list'>
+                     <List bulleted as='h3' size='big'>
+                        <List.Item>Search Articles Across the Web</List.Item>
+                        <List.Item>Customize Topics to Follow</List.Item>
+                        <List.Item>Easily Access Your Categories for Future Reference</List.Item>
+                        <List.Item>Completely Free to Join!</List.Item>
+                     </List>
                 </div>
-
-            </div>
+                
+                    <Link to='/signup'>
+                        <Button 
+                            type='submit'
+                            size='massive'
+                            primary
+                        >
+                            Join Today   
+                        </Button>
+                    </Link>
+                    
+                        
+                        
+                </Grid.Column>
+            </Grid>
+            
+            
 
         </>
     );
