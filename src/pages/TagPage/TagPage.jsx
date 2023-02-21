46ed57da5b16dd1f1090af5c74618bbadd8bdabf
jmaksimik <jmaks@me.com>
2023-02-20 22:34:43 -0500

    Establish initial Guardian News API connection

--- src/pages/TagPage/TagPage.jsx
+++ src/pages/TagPage/TagPage.jsx
@@ -1,51 +1,60 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import {useParams} from 'react-router-dom';
-import {Grid} from 'semantic-ui-react';
+import { useParams } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
 
 import PageHeader from '../../components/PageHeader/PageHeader';
 import ArticleList from '../../components/ArticleList/ArticleList';
 
 
-export default function TagPage({loggedUser, handleLogout, removeBookmark, liftApiKeywords, bookmarkStatus, addBookmark, bookmarkExists, bookmarks, getBookmarks}) {
+
+export default function TagPage({ loggedUser, handleLogout, removeBookmark, liftApiKeywords, bookmarkStatus, addBookmark, bookmarkExists, bookmarks, getBookmarks }) {
     const [nytArticles, setNytArticles] = useState([])
-    const {tagName} = useParams();
+    const [guardianArticles, setGuardianArticles] = useState([]);
+    const { tagName } = useParams();
     const pageTitle = tagName.replace('-', ' ').toUpperCase();
+    const guardianKeyWords = tagName.replace('-', '%20AND%20')
+    
 
     useEffect(() => {
         liftApiKeywords(tagName);
-        let nytURL= `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${tagName}&sort=newest&api-key=RPBVKJx9BvLEOAcgjyFJYW3axTX2Ox1A`
+        let nytURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${tagName}&sort=newest&api-key=RPBVKJx9BvLEOAcgjyFJYW3axTX2Ox1A`
+        let guardianURL = `https://content.guardianapis.com/search?q=${guardianKeyWords}&api-key=d12eebf2-426a-4265-b2f9-42fb638338ad&show-fields=thumbnail&type=article`
 
         async function makeApiCall() {
-            try{
-                const responseJSON= await fetch(nytURL);
-                const data = await responseJSON.json();
-                setNytArticles(data.response.docs)
-            } catch(err) {
+            try {
+                const nytResponseJSON = await fetch(nytURL);
+                const guardianResponseJSON = await fetch(guardianURL);
+                const nytData = await nytResponseJSON.json();
+                const guardianData = await guardianResponseJSON.json();
+                setNytArticles(nytData.response.docs);
+                setGuardianArticles(guardianData);
+            } catch (err) {
                 console.log(err)
             }
         }
         makeApiCall();
         getBookmarks();
     }, [])
-    
+
     return (
         <>
-        <PageHeader loggedUser={loggedUser} handleLogout={handleLogout} />
-        <Grid textAlign='center'>
-            <Grid.Column style={{maxWidth: 900}}>
-                <h1>{pageTitle} LATEST NEWS</h1>
-                <ArticleList 
-                    nytArticles={nytArticles} 
-                    removeBookmark={removeBookmark} 
-                    bookmarkStatus={bookmarkStatus} 
-                    addBookmark={addBookmark} 
-                    bookmarkExists={bookmarkExists} 
-                    bookmarks={bookmarks} 
-                    getBookmarks={getBookmarks} />
-            </Grid.Column>
-        </Grid>
-        
+            <PageHeader loggedUser={loggedUser} handleLogout={handleLogout} />
+            <Grid textAlign='center'>
+                <Grid.Column style={{ maxWidth: 900 }}>
+                    <h1>{pageTitle} LATEST NEWS</h1>
+                    <ArticleList
+                        nytArticles={nytArticles}
+                        removeBookmark={removeBookmark}
+                        bookmarkStatus={bookmarkStatus}
+                        addBookmark={addBookmark}
+                        bookmarkExists={bookmarkExists}
+                        bookmarks={bookmarks}
+                        getBookmarks={getBookmarks}
+                        guardianArticles={guardianArticles} />
+                </Grid.Column>
+            </Grid>
+
         </>
     )
 }
